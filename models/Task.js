a63d28eef3b6626c9585acca198638bd9bc37cce
Kevin Salcedo <kevsalcedo@gmail.com>
2020-04-05 20:52:47 -0500

    CRU operations for tasks

---
+++ models/Task.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  taskList: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "tasklist",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  startDate: {
+    type: Date,
+  },
+  endDate: {
+    type: Date,
+  },
+  createDate: {
+    type: Date,
+  },
+  // TODO: subtasks?
+  // TODO: index in list?
+});
+
+module.exports = Task = mongoose.model("task", TaskSchema);
