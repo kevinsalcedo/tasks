77124ccd39c0b638585d5900223926e1956c0834
Kevin Salcedo <kevsalcedo@gmail.com>
2020-05-14 21:40:00 -0500

    added display changes for due dates

--- client/src/components/layout/containers/TaskCard.js
+++ client/src/components/layout/containers/TaskCard.js
@@ -1,13 +1,33 @@
-import React from "react";
-import { Box, CheckBox, Text, Menu } from "grommet";
+import React, { useState } from "react";
+import { Box, CheckBox, Collapsible, Text, Menu } from "grommet";
 import { MoreVertical } from "grommet-icons";
 import moment from "moment";
 
 const TaskCard = ({ task, onDeleteOpen, onDeleteClose }) => {
+  const [open, toggleOpen] = useState(false);
+
   const renderDueDate = () => {
     let innerText = "No due date";
     if (task.endDate) {
-      innerText = moment(task.endDate).format("MM/DD/YYYY hh:mm a");
+      const now = moment().startOf("day");
+      const dueDate = moment(task.endDate).startOf("day");
+      const timeEnabled = task.endTimeEnabled;
+
+      //Day comparisons for display
+      const diff = dueDate.diff(now, "days");
+      if (diff < 0) {
+        innerText = "Past due";
+      } else if (diff === 0) {
+        innerText =
+          "Today" + (timeEnabled ? ` ${dueDate.format("hh:mm a")}` : "");
+      } else if (diff === 1) {
+        innerText =
+          "Tomorrow" + (timeEnabled ? ` ${dueDate.format("hh:mm a")}` : "");
+      } else if (diff < 7) {
+        innerText = dueDate.format("ddd" + (timeEnabled ? " hh:mm a" : ""));
+      } else {
+        innerText = dueDate.format("MMM Do");
+      }
     }
     return <Text size='xsmall'>{innerText}</Text>;
   };
@@ -16,33 +36,33 @@ const TaskCard = ({ task, onDeleteOpen, onDeleteClose }) => {
     { label: "Delete Task", onClick: onDeleteOpen },
   ];
   return (
-    <Box
-      direction='row'
-      pad={{ horizontal: "medium", vertical: "xxsmall" }}
-      background='light-1'
-      elevation='small'
-      gap='small'
-      justify='between'
-      align='center'
-      flex={false}
-      border={{
-        color: task.taskList.color,
-        side: "left",
-        size: "large",
-        style: "solid",
-      }}
-    >
-      <Box direction='row' gap='small' align='center'>
-        <CheckBox checked={task.completed} />
-        <Box>
+    <Box>
+      <Box
+        direction='row'
+        pad={{ horizontal: "medium", vertical: "xxsmall" }}
+        background='light-1'
+        elevation='small'
+        gap='small'
+        justify='between'
+        align='center'
+        flex={false}
+        border={{
+          color: task.taskList.color,
+          side: "left",
+          size: "large",
+          style: "solid",
+        }}
+      >
+        <Box direction='row' gap='small' align='center'>
+          <CheckBox checked={task.completed} />
           <Box>
             <Text truncate>{task.name}</Text>
           </Box>
-          {renderDueDate()}
         </Box>
-      </Box>
-      <Box direction='row' gap='small' align='center'>
-        <Menu icon={<MoreVertical />} items={menuItems} hoverIndicator />
+        <Box direction='row' gap='small' align='center'>
+          {renderDueDate()}
+          <Menu icon={<MoreVertical />} items={menuItems} hoverIndicator />
+        </Box>
       </Box>
     </Box>
   );
